8f81e89b1edd3b69c785e191532fe30d9fe23aba
bcklin <crosslinktest4321@gmail.com>
2024-06-21 16:32:42 -0700

    Add Primeagen key binds

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -7,6 +7,46 @@ print ("Loading custom keybinds")
 -- vim.api.nvim_set_hl(0, "NormalFloat", {bg="none"})
 
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git )	-- think of as git status
+-- Likle VC code alt+up|down arrow keys / move selected line values up or down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv" )
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv" )
+-- half page jumping keep cursor on middle line 'zz' center this line
+vim.keymap.set("n", "<C-d>", "<C-d>zz" )
+vim.keymap.set("n", "<C-u>", "<C-u>zz" )
+--move next line to the end of current line
+vim.keymap.set("n", "J", "mzJ`z" )
+--keep search line centered when possible
+vim.keymap.set("n", "n", "nzzzv" )
+vim.keymap.set("n", "N", "Nzzzv" )
+--replace highlighted word while perserving ?buffer? by using void buffer
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- asbjornHaland -> yank to "+" register
+-- vim.keymap.set("n", "<leader>y", "\"+y")
+-- vim.keymap.set("v", "<leader>y", "\"+y")
+-- vim.keymap.set("n", "<leader>Y", "\"+Y")
+-- for void register
+-- vim.keymap.set("n", "<leader>d", "\"_d")
+-- vim.keymap.set("v", "<leader>d", "\"_d")
+-- vim.keymap.set("v", "<leader>d", "\"_d")
+
+-- vertical replace. Save the changes with Esc. ctrl+c doesnt save the changes
+-- vim.keymap.set("i", "<C-c>", "<Esc>")
+-- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+-- quick fix navigation
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+
+-- Replace word currently on
+-- vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- make file executable
+-- vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 --******************************************* Harpoon 2 setup ***************************************
 local harpoon = require("harpoon")
